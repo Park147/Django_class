d23a5e6616b8641846d38d832424c2c83eecf231
Park147 <54125201+Park147@users.noreply.github.com>
2023-07-20 14:15:48 +0900

    Delete pyboard directory
    삭제

--- pyboard/board/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from board.models import Board
-
-# Register your models here.
-
-class BoardAdmin(admin.ModelAdmin):
-    list_display=("title", 'writer', 'hit', 'post_date')
-    
-admin.site.register(Board, BoardAdmin)
