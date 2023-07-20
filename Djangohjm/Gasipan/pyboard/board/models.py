adff08b1822f197902fbf3c4e878f82e4f1ba326
Park147 <54125201+Park147@users.noreply.github.com>
2023-07-20 14:13:34 +0900

    230718~20 장고class

---
+++ Djangohjm/Gasipan/pyboard/board/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from _datetime import datetime
+
+
+# Create your models here.
+class Board(models.Model):
+    bno=models.AutoField(primary_key=True)
+    writer=models.CharField(null=False, max_length=50)
+    title=models.CharField(null=False,max_length=120)
+    content=models.TextField(null=False)
+    hit=models.IntegerField(default=0)
+    post_date=models.DateTimeField(default=datetime.now, blank=True)
+    filename=models.CharField(null=True, blank=True, default='', max_length=500)
+    filesize=models.IntegerField(default=0)
+    down=models.IntegerField(default=0)
+    
+    def hit_up(self):
+        self.hit+=1
+        
+    def down_up(self):
+        self.down+=1
+        
+class Comment(models.Model):
+    cno=models.AutoField(primary_key=True)
+    bno=models.IntegerField(null=False)
+    writer=models.CharField(null=False, max_length=50)
+    content=models.TextField(null=False)
+    post_date=models.DateTimeField(default=datetime.now, blank=True)
+    
+class Book(models.Model):
+    bno=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=250) 
+    author=models.CharField(max_length=500)
+    price=models.IntegerField(default=0)
+    point=models.FloatField(default=0.0)
+    
+       
---
+++ pyboard/board/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from board.models import Board
+
+# Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display=("title", 'writer', 'hit', 'post_date')
+    
+admin.site.register(Board, BoardAdmin)
